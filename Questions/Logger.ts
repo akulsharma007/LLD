739b88f90844c8e600845f5850ce9f6e320c8cec
Akul Sharma <akul.sharma007@gmail.com>
2026-07-29 00:24:07 +0200

    Create Logger.ts

---
+++ Questions/Logger.ts
@@ -0,0 +1,2 @@
+/* Q2. Design a Logger that can write to different outputs: console, file (simulate it), and a silent/no-op mode. 
+The output target should be changeable after the logger is created. */
